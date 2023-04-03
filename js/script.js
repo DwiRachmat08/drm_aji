b10127a4c47a8a02be43f875e442e5b15c4aed8d
DwiRachmat08 <dwirachmatmulyo@gmail.com>
2023-04-03 13:31:50 +0700

    perubahan tampilan

--- js/script.js
+++ js/script.js
@@ -1,29 +1,85 @@
 // alert('tes')
 
-document.addEventListener('DOMContentLoaded', () => {
-    // alert('coba');
-    AOS.init();
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // alert('coba');
+  AOS.init();
+});
 
-let toggleBtn = document.querySelector('.toggle_btn');
-let toggleBtnIcon = document.querySelector('.toggle_btn i');
-let dropDownMenu = document.querySelector('.dropdown_menu');
-let text = document.getElementById('text');
+let toggleBtn = document.querySelector(".toggle_btn");
+let toggleBtnIcon = document.querySelector(".toggle_btn i");
+let dropDownMenu = document.querySelector(".dropdown_menu");
+let text = document.getElementById("text");
 
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
 
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    console.log('yoi');
-
-    // text.style.marginTop = value * 2.5 + 'px';
-})
+  // text.style.marginTop = value * 2.5 + 'px';
+});
 
 toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open');
+  dropDownMenu.classList.toggle("open");
 
-    let isOpen = dropDownMenu.classList.contains('open');
+  let isOpen = dropDownMenu.classList.contains("open");
 
-    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
-}
+  toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
+};
 
+// TYPERWRITE
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
 
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
